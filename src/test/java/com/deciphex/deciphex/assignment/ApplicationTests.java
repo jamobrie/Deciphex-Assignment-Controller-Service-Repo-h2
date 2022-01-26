c8e7cbab108dca4555a1083f9a6ffd795277ac5f
James O'Brien <jamesobrien442@gmail.com>
2022-01-26 16:26:23 +0000

    Initial Code Setup To Verify App Is Able To Start Up

---
+++ src/test/java/com/deciphex/deciphex/assignment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deciphex.deciphex.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
